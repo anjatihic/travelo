616cdc0e85054fa16db03df96e662a24cc40079c
Anja Tihić <atihic99@gmail.com>
2024-03-11 16:08:35 +0100

    Spring initial

---
+++ src/main/java/hr/tvz/travelo/TraveloApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.travelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveloApplication.class, args);
+	}
+
+}
