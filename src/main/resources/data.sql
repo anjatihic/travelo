17a3e6461da9f698dd142669aa70dd5352f8d431
Anja Tihić <atihic99@gmail.com>
2024-03-12 12:01:44 +0100

    Create travel group model

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into plan_type (NAME, STATUS)
-values ('Accommodation', 1 );
+values ('Accommodation', 1);
 
 insert into travel_group(code, name, created_at, status, trip_start, trip_end)
-values ( '00000', 'England tour', '2024-3-12', 1, '2025-4-4', '2025-5-5' ),
+values ('00000', 'England tour', '2024-3-12', 1, '2025-4-4', '2025-5-5'),
        ('00001', 'Plitvička jezera', '2024-3-12', 1, '2024-8-8', '2024-8-8');
