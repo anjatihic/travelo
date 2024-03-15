59ba15dc9cb29a208e08e7606ca35b31ee8ad0c4
Anja Tihić <atihic99@gmail.com>
2024-03-15 11:12:27 +0100

    Hide navigation when on register or login page

--- view/src/app/app.component.ts
+++ view/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {NavigationEnd, Router} from "@angular/router";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'view';
+  showNavigation: boolean = false;
+
+  constructor(private router: Router) {
+    router.events.subscribe((val) =>{
+      if(val instanceof NavigationEnd) {
+        this.showNavigation = !(val.url == '/login' || val.url == '/register');
+      }
+    })
+  }
 }
